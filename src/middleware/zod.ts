b2848db461f561af064344ded09b85997cac038d
Wendell Lance <wendell.ravago@linoflaptech.com>
2025-08-30 14:51:25 +0800

    add zod validation middleware

---
+++ src/middleware/zod.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express'
+import { z, ZodError } from 'zod'
+
+export const validate = <T>(schema: z.ZodSchema<T>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse(req.body);
+            next()
+        }
+        catch(err: unknown) {
+            if (err instanceof ZodError) {
+                const errorMessages = err.issues.map((issue) => ({
+                    [issue.path.join('.')]: `${issue.message}`,
+                }))
+                console.log(errorMessages)
+                res.status(400).json({success: false, data: {}, message: errorMessages})
+            }
+            else {
+                const error = err as Error
+                res.status(500).json({success: false, data: {}, message: error.message })
+            }   
+        }
+    }
+}
