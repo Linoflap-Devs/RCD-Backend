57a8f962a65e28bf20c985735c8bbfe0abe14e74
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-01-19 12:09:20 +0800

    update db schema for agent registration

--- src/db/db-types.ts
+++ src/db/db-types.ts
@@ -159,16 +159,16 @@ export interface TblAgentReferral {
 
 export interface TblAgentRegistration {
   Address: string;
-  AddressEmergency: string;
-  AffiliationDate: Date;
+  AddressEmergency: string | null;
+  AffiliationDate: Date | null;
   AgentCode: string;
   AgentRegistrationID: Generated<number>;
-  AgentTaxRate: number;
+  AgentTaxRate: number | null;
   Birthdate: Date;
   Birthplace: string | null;
-  CivilStatus: string;
-  ContactEmergency: string;
-  ContactNumber: string;
+  CivilStatus: string | null;
+  ContactEmergency: string | null;
+  ContactNumber: string | null;
   DivisionID: string | null;
   DSHUDNumber: string | null;
   EmployeeIDNumber: string | null;
@@ -179,7 +179,7 @@ export interface TblAgentRegistration {
   LastUpdate: Generated<Date>;
   MiddleName: string;
   PagIbigNumber: string | null;
-  PersonEmergency: string;
+  PersonEmergency: string | null;
   PhilhealthNumber: string | null;
   PositionID: number | null;
   PRCNumber: string | null;
