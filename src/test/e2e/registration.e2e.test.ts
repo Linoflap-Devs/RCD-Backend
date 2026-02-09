e7b108956ff07e7f09656adfff83cb4cd9e2ac42
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-02-09 15:56:32 +0800

    add e2e test for email invitation registration

--- src/test/e2e/registration.e2e.test.ts
+++ src/test/e2e/registration.e2e.test.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createAdmin, createSD } from '../helpers/users.helpers'
+import { createAdmin, createSD, createUM } from '../helpers/users.helpers'
 import { seedDivisions, seedPositions } from '../helpers/seed.helpers'
 import request from 'supertest'
 import { db } from '../../db/db'
@@ -8,6 +8,7 @@ import { truncateAllTables, truncateTables } from '../helpers/db.helpers'
 // Import your actual routes
 import authRouter from '../../routes/auth.routes'
 import agentRouter from '../../routes/agents.routes'
+import usersRouter from '../../routes/users.routes'
 import path from 'path'
 import fs from 'fs'
 import { IAgentRegistrationListItem } from '../../types/auth.types'
@@ -23,6 +24,7 @@ app.use(cookieParser())
 // Mount your routes
 app.use('/api/auth', authRouter)
 app.use('/api/agents', agentRouter)
+app.use('/api/users', usersRouter)
 
 describe('Mobile Registration test', () => {
 
@@ -169,6 +171,169 @@ describe('Mobile Registration test', () => {
 
     afterAll(async () => {
         const cleanup = await truncateAllTables()
+    })
+})
+
+describe('Email Invitation test', () => {
+
+    let agentUM: ReturnType<typeof request.agent> = request.agent(app)
+    let agentSA: ReturnType<typeof request.agent> = request.agent(app)
+    let agentRegistrationId = 0
+    let inviteToken = ''
+
+     it('should create divisions', async () => {
+        const result = await seedDivisions()
+
+        expect(result.success).toBe(true)
+    })
+
+    it('should create positions', async () => {
+        const result = await seedPositions()
+
+        expect(result.success).toBe(true)
+    })
+
+    it('should create an UM user', async () => {
+        const umUser = await createUM(1)
+
+        expect(umUser.success).toBe(true)
+    })
+
+    it('should create an SA user', async () => {
+        const saUser = await createAdmin()
+
+        expect(saUser.success).toBe(true)
+    })
+
+    it('should login the UM', async () => {
+        const response = await agentUM
+            .post('/api/auth/login-agent')
+            .send({
+                email: 'um@gmail.com',
+                password: process.env.TESTING_PW || 'password'
+            })
+        console.log("status code: ", response.statusCode)
+        console.log("req body: ", response.body)
+        console.log("cookies: ", response.headers['set-cookie'])
+            
+        expect(response.body.success).toBe(true)
+    })
+
+    it('should send an invite to a new email', async () => {
+        const response = await agentUM
+            .post('/api/users/invite-user')
+            .send({
+                email: 'sp@thisemaildoesnotexist.com'
+            })
+        
+        expect(response.body.success).toBe(true)
+
+        inviteToken = response.body.data.inviteToken
+    }, 30000)
+
+    it('should submit a new registration form with the token', async () => {
+        const response = await request(app)
+            .post('/api/auth/register-invite')
+            .field('firstName', 'John')
+            .field('middleName', 'Doe')
+            .field('lastName', 'Doe')
+            .field('gender', 'Male')
+            .field('birthdate', '1990-01-01')
+            .field('address', '123 Main St')
+            .field('email', 'sp@thisemaildoesnotexist.com')
+            .field('password', process.env.TESTING_PW || 'password')
+            .field('inviteToken', inviteToken)
+
+        expect(response.body.success).toBe(true)
+    })
+
+    it('should see the registration in the UM list', async () => {
+        const response = await agentUM
+            .get('/api/users/invited')
+            .query({page: 1, pageSize: 5})
+
+        expect(response.body.success).toBe(true)
+        expect(response.body.data.some((agent: IAgentRegistrationListItem) => agent.Email === 'sp@thisemaildoesnotexist.com')).toBe(true)
+    })
+
+    it('should approve the registration (UM)', async () => {
+        const response = await agentUM
+            .post('/api/auth/approve-invite-um')
+            .send({inviteToken: inviteToken})
+        
+        expect(response.body.success).toBe(true)
+        console.log(response.body)
+    })
+
+    it('should reject the unapproved agent login', async () => {
+        const response = await request(app)
+            .post('/api/auth/login-agent')
+            .send({
+                email: 'sp@thisemaildoesnotexist.com',
+                password: process.env.TESTING_PW || 'password'
+            })
+        console.log("status code: ", response.statusCode)
+        console.log("req body: ", response.body)
+            
+        expect(response.body.success).toBe(false)
+    })
+
+    it('should login an sales admin', async () => {
+        const response = await agentSA
+            .post('/api/auth/login-employee')
+            .send({
+                username: 'admin',
+                password: process.env.TESTING_PW || 'password'
+            })
+        console.log("status code: ", response.statusCode)
+        console.log("req body: ", response.body)
+            
+        expect(response.body.success).toBe(true)
+    })
+
+    it('should see the agent from the registration list', async () => {
+
+        const response = await agentSA
+            .get('/api/agents/registrations')
+            .query({page: 1, pageSize: 5})
+
+        console.log(response.statusCode)
+        console.log(response.body.data)
+
+        expect(response.body.success).toBe(true)
+        expect(response.body.data.result.some((agent: IAgentRegistrationListItem) => agent.Email === 'sp@thisemaildoesnotexist.com')).toBe(true)
+
+        agentRegistrationId = response.body.data.result[0].AgentRegistrationID
+    })
+
+    it('should approve the agent registration', async () => {
+        const response = await agentSA
+            .post('/api/auth/approve-registration')
+            .send({agentRegistrationId: agentRegistrationId})
+
+        expect(response.body.success).toBe(true)
+        console.log(response.body)
+    })
+
+    it('should login the agent', async () => {
+        const response = await request(app)
+            .post('/api/auth/login-agent')
+            .send({
+                email: 'sp@thisemaildoesnotexist.com',
+                password: process.env.TESTING_PW || 'password'
+            })
+        console.log("status code: ", response.statusCode)
+        console.log("req body: ", response.body)
+        console.log("cookies: ", response.headers['set-cookie'])
+            
+        expect(response.body.success).toBe(true)
+    })
+
+
+
+    afterAll(async () => {
+        const cleanup = await truncateAllTables()
         await db.destroy()
     })
+
 })
