c08e938a6e1344851d14cc77c8b030af20528f25
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-02-13 15:32:18 +0800

    add unit test for broker gov id edit controller

--- src/test/unit/controller/users.controller.test.ts
+++ src/test/unit/controller/users.controller.test.ts
@@ -1,5 +1,5 @@
-import { editAgentGovIdsController } from "../../../controller/users.controller";
-import { editAgentGovIdsService } from "../../../service/users.service";
+import { editAgentGovIdsController, editBrokerGovIdsController } from "../../../controller/users.controller";
+import { editAgentGovIdsService, editBrokerGovIdsService } from "../../../service/users.service";
 import { Request, Response } from "express";
 
 jest.mock('../../../service/users.service');
@@ -44,6 +44,50 @@ describe('Controller Tests', () => {
             (editAgentGovIdsService as jest.Mock).mockResolvedValue({ success: true, data: [] });
 
             const result = await editAgentGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(200)
+        })
+    })
+
+    describe('editBrokerGovIdsController', () => {
+
+        let req: Partial<Request>;
+        let res: Partial<Response>;
+
+        beforeEach(() => {
+            req = {
+                session: { userID: 1, sessionID: 1, userRole: '', isVerified: 1 },
+                body: [{ IdType: 'PRCNumber', IdNumber: '123' }]
+            };
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+        });
+
+        it('should return 401 if no session', async () => {
+
+            req.session = undefined;
+
+            const result = await editBrokerGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(401)
+        })
+
+        it('should return 401 if no user id', async () => {
+
+            req.session = { } as any;
+
+            const result = await editBrokerGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(401)
+        })
+
+        it('should return 200 if success', async () => {
+
+            (editBrokerGovIdsService as jest.Mock).mockResolvedValue({ success: true, data: [] });
+
+            const result = await editBrokerGovIdsController(req as Request, res as Response);
 
             expect(res.status).toHaveBeenCalledWith(200)
         })
