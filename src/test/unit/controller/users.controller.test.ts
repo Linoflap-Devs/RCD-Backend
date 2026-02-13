2dd233e4c710dd268eac450debef5a79ddd45380
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-02-13 09:37:31 +0800

    add unit test for edit gov id controller

---
+++ src/test/unit/controller/users.controller.test.ts
@@ -0,0 +1,51 @@
+import { editAgentGovIdsController } from "../../../controller/users.controller";
+import { editAgentGovIdsService } from "../../../service/users.service";
+import { Request, Response } from "express";
+
+jest.mock('../../../service/users.service');
+
+describe('Controller Tests', () => {
+    describe('editAgentGovIdsController', () => {
+
+        let req: Partial<Request>;
+        let res: Partial<Response>;
+
+        beforeEach(() => {
+            req = {
+                session: { userID: 1, sessionID: 1, userRole: '', isVerified: 1 },
+                body: [{ IdType: 'PRCNumber', IdNumber: '123' }]
+            };
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+        });
+
+        it('should return 401 if no session', async () => {
+
+            req.session = undefined;
+
+            const result = await editAgentGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(401)
+        })
+
+        it('should return 401 if no user id', async () => {
+
+            req.session = { } as any;
+
+            const result = await editAgentGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(401)
+        })
+
+        it('should return 200 if success', async () => {
+
+            (editAgentGovIdsService as jest.Mock).mockResolvedValue({ success: true, data: [] });
+
+            const result = await editAgentGovIdsController(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(200)
+        })
+    })
+})
