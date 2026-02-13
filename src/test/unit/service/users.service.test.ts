dca4de92f5a7851895f98d431108a4344f51188c
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-02-13 15:32:12 +0800

    add unit test for broker gov id edit service

--- src/test/unit/service/users.service.test.ts
+++ src/test/unit/service/users.service.test.ts
@@ -1,5 +1,5 @@
-import { editAgentGovIds, findAgentDetailsByUserId } from "../../../repository/users.repository";
-import { editAgentGovIdsService } from "../../../service/users.service";
+import { editAgentGovIds, editBrokerGovIds, findAgentDetailsByUserId, findBrokerDetailsByUserId } from "../../../repository/users.repository";
+import { editAgentGovIdsService, editBrokerGovIdsService } from "../../../service/users.service";
 
 jest.mock('../../../repository/users.repository');
 jest.mock('../../../repository/agents.repository');
@@ -38,6 +38,41 @@ describe('Service Tests', () => {
             expect(result.success).toBe(true);
             expect(findAgentDetailsByUserId).toHaveBeenCalledWith(1);
             expect(editAgentGovIds).toHaveBeenCalledWith(1, [{IdType: 'SSSNumber', IdNumber: 'SSS123456789'}]);
+        });
+    })
+
+    describe('editBrokerGovIdsService', () => {
+        it('rejects missing broker', async () => {
+            (findBrokerDetailsByUserId as jest.Mock).mockResolvedValue({
+                success: false
+            });
+            
+            (editBrokerGovIds as jest.Mock).mockResolvedValue({
+                success: true,
+                data: [{ IdType: 'PRCNumber', IdNumber: '123' }]
+            });
+
+            const result = await editBrokerGovIdsService(1, [{IdType: 'SSSNumber', IdNumber: 'SSS123456789'}]);
+            
+            expect(result.success).toBe(false);
+        });
+
+        it('edits gov ids', async () => {
+            (findBrokerDetailsByUserId as jest.Mock).mockResolvedValue({
+                success: true,
+                data: { BrokerID: 1 }
+            });
+            
+            (editBrokerGovIds as jest.Mock).mockResolvedValue({
+                success: true,
+                data: [{ IdType: 'PRCNumber', IdNumber: '123' }]
+            });
+
+            const result = await editBrokerGovIdsService(1, [{IdType: 'SSSNumber', IdNumber: 'SSS123456789'}]);
+            
+            expect(result.success).toBe(true);
+            expect(findBrokerDetailsByUserId).toHaveBeenCalledWith(1);
+            expect(editBrokerGovIds).toHaveBeenCalledWith(1, [{IdType: 'SSSNumber', IdNumber: 'SSS123456789'}]);
         });
     })
   
