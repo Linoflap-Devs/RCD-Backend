2afdcec0cdc0230fc60ee606ba28f2a6132e7c0f
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-02-13 15:31:48 +0800

    add unit test for broker gov id edit

--- src/test/unit/repository/users.repository.test.ts
+++ src/test/unit/repository/users.repository.test.ts
@@ -1,7 +1,7 @@
 import { db } from "../../../db/db"
-import { editAgentGovIds, getAgentGovIds } from "../../../repository/users.repository"
+import { editAgentGovIds, editBrokerGovIds, getAgentGovIds, getBrokerGovIds } from "../../../repository/users.repository"
 import { truncateAllTables } from "../../helpers/db.helpers"
-import { createUM } from "../../helpers/users.helpers"
+import { createHandsOffBroker, createUM } from "../../helpers/users.helpers"
 describe('Users Repository Test', () => {
     describe('editAgentGovId', () => {
         it('should create a unit manager', async () => {
@@ -47,6 +47,58 @@ describe('Users Repository Test', () => {
             expect(result.success).toBe(true)
 
             const verifyResult = await getAgentGovIds(1)
+
+            expect(verifyResult.success).toBe(true)
+            const sortByIdType = (a: { IdType: string, IdNumber: string | null }, b: { IdType: string, IdNumber: string | null }) => a.IdType.localeCompare(b.IdType);
+
+            expect(verifyResult.data.sort(sortByIdType)).toEqual(updateData.sort(sortByIdType));
+        })
+    })  
+
+    describe('editBrokerGovId', () => {
+        it('should create a hands off broker', async () => {
+            const result = await createHandsOffBroker()
+
+            expect(result.success).toBe(true)
+        })
+
+        it('should edit known ID columns', async () => {
+            const updateData: {IdType: string, IdNumber: string}[] = [
+                {
+                    IdType: 'SSSNumber',
+                    IdNumber: 'SSS123456789'
+                },
+                {
+                    IdType: 'PhilhealthNumber',
+                    IdNumber: 'PH123456789'
+                },
+                {
+                    IdType: 'PagIbigNumber',
+                    IdNumber: 'PI123456789'
+                },
+                {
+                    IdType: 'TINNumber',
+                    IdNumber: 'TIN123456789'
+                },
+                {
+                    IdType: 'EmployeeIDNumber',
+                    IdNumber: 'EID123456789'
+                },
+                {
+                    IdType: 'PRCNumber',
+                    IdNumber: 'PRC123456789'
+                },
+                {
+                    IdType: 'DSHUDNumber',
+                    IdNumber: 'DSHUD123456789'
+                },
+            ]
+
+            const result = await editBrokerGovIds(1, updateData)
+
+            expect(result.success).toBe(true)
+
+            const verifyResult = await getBrokerGovIds(1)
 
             expect(verifyResult.success).toBe(true)
             const sortByIdType = (a: { IdType: string, IdNumber: string | null }, b: { IdType: string, IdNumber: string | null }) => a.IdType.localeCompare(b.IdType);
