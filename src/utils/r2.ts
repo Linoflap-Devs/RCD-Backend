ca36735444eab9f5f8d95f1ce7c47fea453b7f25
WRavagoLFT <wendell.ravago@linoflaptech.com>
2026-03-19 15:53:00 +0800

    add utils for r2 uploads

---
+++ src/utils/r2.ts
@@ -0,0 +1,74 @@
+import { PutObjectCommand, PutObjectCommandOutput, S3Client } from "@aws-sdk/client-s3"
+import { QueryResult } from "../types/global.types"
+import { R2ImageUploadResult } from "../types/r2.types"
+
+
+if(!process.env.R2_ENDPOINT) throw new Error('R2_ENDPOINT is not defined')
+if(!process.env.R2_S3_ACCESS_KEY) throw new Error('R2_S3_ACCESS_KEY is not defined')
+if(!process.env.R2_S3_SECRET_KEY) throw new Error('R2_S3_SECRET_KEY is not defined')
+if(!process.env.R2_BUCKET) throw new Error('R2_BUCKET is not defined')
+
+const bucket = process.env.R2_BUCKET
+
+const s3client = new S3Client({
+    region: 'auto',
+    endpoint: process.env.R2_ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.R2_S3_ACCESS_KEY,
+        secretAccessKey: process.env.R2_S3_SECRET_KEY
+    },
+})
+
+const uploadImage = async (bucketName: string, key: string, body: Express.Multer.File): QueryResult<R2ImageUploadResult> => {
+    
+    try {
+        const storageKey = `${key}_${new Date().toISOString()}.${body.originalname.split('.').pop()}`
+
+        const result = await s3client.send(
+            new PutObjectCommand({
+                Bucket: bucketName,
+                Key: storageKey,
+                Body: body.buffer,
+                ContentType: body.mimetype
+            })
+        )
+
+        return {
+            success: true,
+            data: {
+                output: result,
+                storageKey
+            }   
+        }
+    }
+
+    catch(err: unknown){
+        const error = err as Error;
+        return {
+            success: false,
+            data: {} as R2ImageUploadResult,
+            error: {
+                code: 500,
+                message: error.message
+            }
+        }
+    }
+}
+
+export const r2UploadGovId = async (agentRegistrationId: number, file: Express.Multer.File): QueryResult<R2ImageUploadResult> => {
+    const result = await uploadImage(bucket, `identification/reg-${agentRegistrationId}/govid`, file)
+
+    return result
+}
+
+export const r2UploadGovSelfie = async (agentRegistrationId: number, file: Express.Multer.File): QueryResult<R2ImageUploadResult> => {
+    const result = await uploadImage(bucket, `identification/reg-${agentRegistrationId}/selfie`, file)
+
+    return result
+}
+
+export const r2UploadAgentAvatar = async (agentId: number, file: Express.Multer.File): QueryResult<R2ImageUploadResult> => {
+    const result = await uploadImage(bucket, `avatars/user-${agentId}/avatar`, file)
+
+    return result
+}
